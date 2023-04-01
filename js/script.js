ed302638f2125e33a8e58d8294bd3be96974d02c
Nelson Vieira <nelsonronnyr40@outlook.com>
2023-04-01 18:13:51 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function exibir_categoria(categoria) {
+  let elementos = document.getElementsByClassName("box-produto");
+  console.log(elementos);
+  for (var i = 0; i < elementos.length; i++) {
+    console.log(elementos[i].id);
+    if (categoria == elementos[i].id) {
+      elementos[i].style = "display: block";
+    } else elementos[i].style = "display: none";
+  }
+}
+
+let exibir_todos = () => {
+  let elementos = document.getElementsByClassName("box-produto");
+  for (var i = 0; i < elementos.length; i++) {
+    elementos[i].style = "display: block";
+  }
+};
+
+let destaque = (imagem) => {
+  console.log(imagem);
+  if (imagem.width == 240) imagem.width = 120;
+  else imagem.width = 240;
+};
