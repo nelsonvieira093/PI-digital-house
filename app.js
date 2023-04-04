b524fcb141406d9b8f7d8c5b860f2d29d21e3a71
emoreirasj <emoreirasj@gmail.com>
2023-04-03 21:21:04 -0300

    Organização de pastas MVC e app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const porta = 1234;
+
+app.get('/', (req,res) => {
+    res.send('Servidor rodando em: / - porta 1234')
+});
+
+app.listen(porta, () => {
+    console.log('Servidor rodando na porta ${porta}')
+});
