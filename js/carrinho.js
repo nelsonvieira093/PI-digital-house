da1a00c8059ff3d0a69e9cac385bc1e6e38cfb57
LYNEMENDONCA <119332590+LYNEMENDONCA@users.noreply.github.com>
2023-05-21 19:10:02 -0300

    Update carrinho.js

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,1 +1,19 @@
+var resumo = document.getElementById('descricao');
+var qtdeProduto = document.getElementById('qtdeProduto');
+var total = document.getElementById('valorTotal');
+var valorProduto;
+var valorTotalCompra;
+//resumo.innerHTML = '<p><strong>Descrição da caneca</strong></p>' + sessionStorage.nome + '<strong>' + sessionStorage.valor + '</strong>';
 
+qtdeProduto.addEventListener('change',calcularValorCompra);
+
+valorProduto.sessionStorage.valor;
+valorProduto. valorProduto.repleceAll('R$', ' ')
+
+function calcularValorCompra(){
+    valorProduto = Number(valorProduto);
+    valorTotalCompra = valorProduto*qtdeProduto.Value;
+
+    total.innerHTML = '<p><strong> Valor total da compra: </strong></p>' + '<strong>R$ ' + valorTotalCompra + '</strong>';
+
+}
