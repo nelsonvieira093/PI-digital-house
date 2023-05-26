0967c60da2e765795d5856dc3043d8e93b016473
Rafael Donizetti <116230150+rafadonizetti@users.noreply.github.com>
2023-05-25 22:34:57 -0300

    Ajustes Front-end (#2)
    * Ajustes Front
    
    * Atualizações com banco de dados simulados
    
    ---------
    
    Co-authored-by: Victor Del Greco Gomes <victordelgrecogomes@gmail.com>

--- Controllers/index.controller.js
+++ Controllers/index.controller.js
@@ -10,18 +10,49 @@ function requererLoja(req, res){
     res.render('loja')
 };
 
-function requererProdutos(req, res){
-    res.render('produtos')
-};
+
 
 function requererCarrinho(req, res){
+    req.ListarProdutos = res.query
     res.render('carrinho')
 };
+
+
+const {ListarProdutos} = require('../Models/produtos.model')
+
+
+const requererTodosProdutos = (req, res) => {
+    const renderizarProdutos = ListarProdutos();
+    res.render("detalhes_produto",{
+        renderizarProdutos,
+    });
+};
+
+const { obterProdutosPorId } = require('../Models/produtos.model');
+
+function requererProdutosPorId(req, res) {
+
+    
+    const  id  = req.params;
+
+    const ProdutosEncontradoPorId = obterProdutosPorId(id);
+
+    res.render("detalhes_produto",{
+        ProdutosEncontradoPorId
+    });
+}
+
+ 
+/*function requererListarProdutos (req, res){
+    res.render('detalhes_produtos')
+};
+*/
 
 module.exports = {
     requererIndex,
     requererContato,
     requererLoja,
-    requererProdutos,
-    requererCarrinho
+    requererCarrinho,
+    requererTodosProdutos,
+    requererProdutosPorId
 }
